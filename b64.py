0e7df980a39b87a03c890e05f113d203d357972c
madrenodriza <eth211992@gmail.com>
2019-03-20 21:52:32 -0600

    Removed Tweepy dependency. Created own solution.

--- b64.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-
-def encode(string_to_encode):
-    return base64.urlsafe_b64encode(string_to_encode.encode('UTF-8')).decode('ascii')
-
-
-def decode(string_to_decode):
-    return base64.urlsafe_b64decode(string_to_decode).decode('ascii')
