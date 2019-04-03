1e406985bc33e913ab66f327507bb56007eaa8df
madrenodriza <eth211992@gmail.com>
2019-04-02 19:52:26 -0600

    Prepared directory to build distribution wheel.

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from distutils.core import setup
 setup(
-  name = 'MarkovTweets',         # How you named your package folder (MyLib)
-  packages = ['MarkovTweets'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'A Python twitter bot that uses a markov chain based sentence generator to fill in tweets.',   # Give a short description about your library
-  author = 'MadreNodriza',                   # Type in your name
-  author_email = 'enodriza@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/madrenodriza',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/madrenodriza/MarkovTweets/archive/v0.1.tar.gz',    # I explain this later on
-  keywords = ['Twitter', 'MarkovChain', 'Bot'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  name = 'MarkovTweets',
+  packages = ['MarkovTweets'],
+  version = '0.1',
+  license='MIT',
+  description = 'A Python twitter bot that uses a markov chain based sentence generator to fill in tweets.',
+  author = 'MadreNodriza',
+  author_email = 'enodriza@gmail.com',
+  url = 'https://github.com/madrenodriza',
+  download_url = 'https://github.com/madrenodriza/MarkovTweets/archive/v0.1.tar.gz',
+  keywords = ['Twitter', 'MarkovChain', 'Bot'],
+  install_requires=[
           'certifi',
           'chardet',
           'idna',
@@ -20,11 +20,11 @@ setup(
           'urllib3',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
