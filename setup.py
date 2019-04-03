2cee1e6ea62cad6499d92b4145d5d3a9ed0dd2bd
madrenodriza <eth211992@gmail.com>
2019-04-02 20:30:33 -0600

    Added relative imports to be able to work with distribution wheel.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'MarkovTweets',
   packages = ['MarkovTweets'],
-  version = '0.1',
+  version = '0.2',
   license='MIT',
   description = 'A Python twitter bot that uses a markov chain based sentence generator to fill in tweets.',
   author = 'MadreNodriza',
   author_email = 'enodriza@gmail.com',
   url = 'https://github.com/madrenodriza',
-  download_url = 'https://github.com/madrenodriza/MarkovTweets/archive/v0.1.tar.gz',
+  download_url = 'https://github.com/madrenodriza/MarkovTweets/archive/v0.2.tar.gz',
   keywords = ['Twitter', 'MarkovChain', 'Bot'],
   install_requires=[
           'certifi',
